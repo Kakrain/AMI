618419ae53cfc33e086e6c42839b0a015ab4d09a
DenkSchuldt <dschuldt@outlook.com>
2014-11-20 16:02:23 -0500

    Android app

---
+++ interactive/app/src/main/java/ami/interactive/main.java
@@ -0,0 +1,163 @@
+package ami.interactive;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
+
+public class main extends Activity implements SensorEventListener {
+
+    Socket socket;
+    private Button boton;
+    Boolean state = false;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private float mLastX, mLastY, mLastZ;
+    boolean mInitialized = false;
+    private final float NOISE = (float) 2.0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //SocketIO Client
+        socketIOSetUp();
+
+        boton = (Button)findViewById(R.id.boton);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(state) {
+                    boton.setText("Adios!");
+                    state = false;
+                }else{
+                    boton.setText("Hola!");
+                    state = true;
+                }
+            }
+        });
+
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void socketIOSetUp(){
+        try {
+            socket = IO.socket("http://192.168.0.3:5000");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                socket.emit("foo", "hi");
+                socket.disconnect();
+            }
+
+        }).on("event", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+            }
+        }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {}
+        });
+        socket.connect();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        TextView tvX= (TextView)findViewById(R.id.x_axis);
+        TextView tvY= (TextView)findViewById(R.id.y_axis);
+        TextView tvZ= (TextView)findViewById(R.id.z_axis);
+        ImageView iv = (ImageView)findViewById(R.id.image);
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+        if (!mInitialized) {
+            mLastX = x;
+            mLastY = y;
+            mLastZ = z;
+            tvX.setText("0.0");
+            tvY.setText("0.0");
+            tvZ.setText("0.0");
+            mInitialized = true;
+        } else {
+            float deltaX = Math.abs(mLastX - x);
+            float deltaY = Math.abs(mLastY - y);
+            float deltaZ = Math.abs(mLastZ - z);
+            if (deltaX < NOISE) deltaX = (float)0.0;
+            if (deltaY < NOISE) deltaY = (float)0.0;
+            if (deltaZ < NOISE) deltaZ = (float)0.0;
+            mLastX = x;
+            mLastY = y;
+            mLastZ = z;
+            tvX.setText(Float.toString(deltaX));
+            tvY.setText(Float.toString(deltaY));
+            tvZ.setText(Float.toString(deltaZ));
+            iv.setVisibility(View.VISIBLE);
+            if (deltaX > deltaY) {
+                iv.setImageResource(R.drawable.horizontal);
+            } else if (deltaY > deltaX) {
+                iv.setImageResource(R.drawable.vertical);
+            } else {
+                iv.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
